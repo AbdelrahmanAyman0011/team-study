e62708265f3a7727a7fdd4346d40ac1b8514a253
Abdelrahman <abdelrahmanayman0011@gmail.com>
2025-08-04 00:01:15 +0300

    study

---
+++ Bedo/main.py
@@ -0,0 +1,9 @@
+#pyre-strict
+
+def dev(a: int, b: int):
+    return a / b
+
+print("welcome to the division function")
+
+nums: list[int] = [10, 20, 30, 40, 50]
+print(dev(100, 5))
---
+++ main.py
@@ -0,0 +1,15 @@
+#pyre-strict
+
+print("hello")
+nums: list[int] = [1, 2, 3, 4, 5] 
+
+def sum(a: int, b: int) -> int:
+    return a + b
+
+for i in nums:
+    if i == 3:
+        print("Welcome Abdelrahman")
+    else:
+        print("not expected")
+
+print(sum(2, 3))
